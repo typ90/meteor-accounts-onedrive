00469bc9c11a10d8c3615e8f7e5ec313aec34959
Max Ferguson <maxkferg@gmail.com>
2014-09-21 14:31:04 -0700

    Bumped version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "maxkferg:accounts-onedrive",
   summary: "OneDrive OAuth flow",
-  version: "0.3.1",
+  version: "0.4.0",
   git: "https://github.com/maxkferg/meteor-accounts-onedrive.git"
 });
 
