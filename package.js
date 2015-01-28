fb8229d19f6f6231e3da0b439e61af96864aa47d
Max Ferguson <maxkferg@gmail.com>
2015-01-27 17:38:17 -0800

    Added support for expiresAt

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "maxkferg:accounts-onedrive",
   summary: "OneDrive OAuth flow",
-  version: "0.4.0",
+  version: "0.4.1",
   git: "https://github.com/maxkferg/meteor-accounts-onedrive.git"
 });
 
